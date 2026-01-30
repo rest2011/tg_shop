9e72d303f434542531ac310726c84eba29f6edb3
Rinat <hamatrin@gmail.com>
2026-01-30 17:59:15 +0500

    3

--- tg_shop/main.py
+++ tg_shop/main.py
@@ -3,6 +3,8 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 import keyboards.keyboard as kb
 import photo.photo as ph
@@ -11,12 +13,47 @@ TOKEN = '8493031792:AAGBtQCJWl9JcnA-aaEcA2Nb-H8ZgK2nASA'
 dp = Dispatcher()
 bot = Bot(token=TOKEN)
 
+class user_data(StatesGroup):
+    name = State()
+    age = State()
+    mood = State()
+
 # @dp.message(CommandStart())
 # async def start(message: Message):
 #     await message.answer(f"Привет, {message.from_user.full_name}"
 #                          f"\n\nИспользуй команду /help",
 #                          reply_markup=kb.reply_kb2.as_markup(resize_keyboard=True)
 #                          )
+
+@dp.message(F.text == '1')
+async def fio(message: Message, state: FSMContext):
+    await state.set_state(user_data.name)
+    await message.answer('Как тебя зовут?')
+
+@dp.message(user_data.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(user_data.age)
+    await message.answer('Сколько тебе лет?')    
+
+@dp.message(user_data.age)
+async def name(message: Message, state: FSMContext):
+    try:
+        age = int(message.text)
+        await state.update_data(age=age)
+        await state.set_state(user_data.mood)
+        await message.answer('Как ваше настроение?')   
+    except ValueError:
+        await message.answer('Возраст введите числом')
+
+@dp.message(user_data.mood)
+async def mood(message: Message, state: FSMContext):
+    mood = message.text
+    data = await state.get_data()
+    name = data.get('name')     
+    age = data.get('age')  
+    await message.answer(f'Привет. Твои данные: \n {name} \n {age} \n {mood}')
+    await state.clear()
 
 @dp.message(CommandStart())
 async def start(message: Message, bot: Bot):
